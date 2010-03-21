72d28db7e8230fb68a02b6db386225e41d9163e5
Tiago Maluta <maluta@unifei.edu.br>
2010-03-21 09:19:58 +0000

    Novos arquivos

---
+++ src/geolocation.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+#!/bin/python
+
+import sys
+from geopy import geocoders  
+
+APIKEY="ABQIAAAALaTde9gMqHnzLPW58lcFTBRVCP9UC649_MmcUms9CnYxhjIH6hSvAPhEnL5l4nj4RN0QcnwWguiPIg"
+
+class GeoLocation:
+	def __init__(self):
+		g = geocoders.Google(APIKEY);
+
+	def getGeoLocation(self, address):
+		g = geocoders.Google(APIKEY);
+		# fazer try - catch
+		place, (lat, lng) = g.geocode(address)
+		return { 'latitude':lat , 'longitude':lng  } 
+		
+
+
